9fc0e641fdfa7b77c9c08c5475c161e96bf4d61b
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-04-01 11:04:28 +0200

    Created Excel Reader class

---
+++ src/main/java/ApachePOIReader.java
@@ -0,0 +1,37 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ApachePOIReader {
+
+    public static List<Employee> readFile(String path, String sheetName) {
+        XSSFWorkbook workbook;
+        try {
+            workbook = new XSSFWorkbook(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        Iterator<Row> rowIterator;
+        List<Employee> list = new ArrayList<>();
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+        rowIterator = sheet.rowIterator();
+        if (rowIterator.hasNext()) {
+            rowIterator.next();
+            rowIterator.forEachRemaining(row -> {
+                list.add(new Employee(row.getCell(0).toString(),
+                        row.getCell(1).toString(),
+                        LocalDate.parse(row.getCell(2).toString())));
+            });
+        }
+        return list;
+    }
+}
